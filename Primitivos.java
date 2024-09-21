7f6541711e4bbea2ccf9ad32abb68e53c3aa282d
Jaguaratica <kitsuneehh@hotmail.com>
2024-09-21 13:47:41 -0300

    Add files via upload

---
+++ Primitivos.java
@@ -0,0 +1,24 @@
+public class Primitivos{
+    public static void main(String[] args) {
+        // No total são 8 tipos primitivos (char, boolean, double, float), 4 deles do tipo inteiro (byte, short, int, long).
+        // Double - Número de ponto flutuante com mais precisão | Float - Faixa de representação um pouco menor.
+        // String é uma classe, não um tipo primitivo.
+        byte idade = 29;
+
+        boolean eBrasileiro = true;
+
+        float dinheiro = 66.50F;
+
+        double salario = 400.20;
+
+        String nome = "Katia";
+
+        if (dinheiro > 200) {
+            System.out.println("É maior que duzentos");
+        } else if (dinheiro == 200) {
+            System.out.println("É igual a duzentos");
+        } else {
+            System.out.println("É menor que duzentos");
+        }
+    }
+}
