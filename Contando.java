029f911a7d4a3b9bc6f44479f3db6058a46f9167
Jaguaratica <kitsuneehh@hotmail.com>
2024-11-08 20:33:56 -0300

    Update Contando.java

--- Contando.java
+++ Contando.java
@@ -1,7 +1,7 @@
+//* Contando de 1 a 10 com um Laço *//
 public class Contando {
 	public static void main(String[] args) {
 		
-	// contar de 1 a 10
 		for(int i = 1; i <= 10; i++){
 			System.out.print(i);
 		}
